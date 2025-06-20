c76b586621a42e67eadd30a2ec5cc20560c5a2d5
aibuddyofficial <offical.aibuddy@gmail.com>
2025-06-20 09:23:16 +0530

    Create App.js

---
+++ Src /App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, AI Buddy Founder!</h1>
+      <p>This is your first React MVP.</p>
+    </div>
+  );
+}
+
+export default App;
